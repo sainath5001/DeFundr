d4a63bd2fc164e9651cfced140ca7ee1340eeafd
Sainath More <sainathmore501@gmail.com>
2025-06-19 04:48:29 +0000

    Initial commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // This contract is used to get the network configuration for different networks
+    // It will return the price feed address based on the network chain ID
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // Sepolia
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 31337) {
+            // Anvil
+            activeNetworkConfig = getAnvilEthConfig();
+        } else {
+            revert("Unsupported network");
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // Sepolia ETH/USD price feed address
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8); // 2000 USD in 8 decimals
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test{
+
+    FundMe fundMe;
+    
+    function setUp() external {
+         //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+         DeployFundMe deployFundMe = new DeployFundMe();
+         fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18, "Minimum USD should be 5");
+
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.getOwner(), msg.sender, "Owner should be the message sender");
+    }
+
+    function testPriceFeedVersionIsAccurate() public view{
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4, "Price feed version should be 4");
+    }
+}
