18286dd74b2e300217eb88ffe60b23e10e8f099e
Sainath More <sainathmore501@gmail.com>
2025-06-22 01:58:17 +0000

    Add tests for FundMe: fund and withdraw logic

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,10 +8,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 0.1 ETH
+    uint256 constant STARTING_BALANCE = 10 ether; // 10 ETH
+
     function setUp() external {
         //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // Give USER 10 ETH
     }
 
     function testMinimumDollarIsFive() public view {
@@ -19,11 +24,43 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.getOwner(), msg.sender, "Owner should be the message sender");
+        assertEq(
+            fundMe.getOwner(),
+            msg.sender,
+            "Owner should be the message sender"
+        );
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4, "Price feed version should be 4");
+    }
+
+    function testFundFailswithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund(); // Sending 1 ETH, which is less than the minimum of 5 USD
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}(); // Sending more than 5 USD worth of ETH
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}(); // Sending more than 5 USD worth of ETH
+        address funder = fundMe.getFunder(0); // Get the first funder
+        assertEq(funder, USER, "Funder should be the user who sent the funds");
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}(); // USER funds the contract
+
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw(); // USER tries to withdraw, should fail
     }
 }
